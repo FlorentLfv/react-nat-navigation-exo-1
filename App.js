3f4bc7cfae68d7eeada12b76452c80c7fa648714
Florent <florentlfv@gmail.com>
2022-05-16 14:36:21 +0200

    exercice 1

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+const ImageIcon = () => {
+  return (
+    <Ionicons name="image" size={30}  />
+  )
+}
+
+const TextIcon = () => {
+  return (
+    <Ionicons name="text" size={30}  />
+  )
+}
+
+const ImageScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: 'https://i.pinimg.com/736x/1b/17/4e/1b174ef0ad95d628cba65ffe1216b1f9.jpg' }} style={styles.image} />
+    </View>
+  );
+};
+
+const TextScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Le chat.</Text>
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer style={styles.container}>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon:() => {
+            let iconName;
+
+            if(route.name == "Image"){
+              iconName = "home"
+
+            }else if( route.name == "Texte"){
+              iconName = "image"
+            }
+            return<Ionicons name={iconName} color="red" size={30}  />
+          }
+        })}
+
+        >
+        <Tab.Screen name="Image" component={ImageScreen} />
+        <Tab.Screen name="Texte" component={TextScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +66,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 200,
+    height: 300,
+    marginBottom: 50,
+  },
+  text: {
+    fontSize: 20,
+  }
 });
